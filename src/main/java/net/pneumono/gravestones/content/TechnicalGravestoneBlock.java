e4f67ced9a848f647ca67e444373ca5c86b1f538
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2024-07-26 00:14:39 +0100

    Prevent FakePlayers using gravestones

--- src/main/java/net/pneumono/gravestones/content/TechnicalGravestoneBlock.java
+++ src/main/java/net/pneumono/gravestones/content/TechnicalGravestoneBlock.java
@@ -1,6 +1,7 @@
 package net.pneumono.gravestones.content;
 
 import com.mojang.serialization.MapCodec;
+import net.fabricmc.fabric.api.entity.FakePlayer;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
@@ -169,7 +170,7 @@ public class TechnicalGravestoneBlock extends BlockWithEntity implements Waterlo
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        if (!world.isClient() && world.getBlockEntity(pos) instanceof TechnicalGravestoneBlockEntity gravestone) {
+        if (!world.isClient() && !(player instanceof FakePlayer) && world.getBlockEntity(pos) instanceof TechnicalGravestoneBlockEntity gravestone) {
             ProfileComponent graveOwner = gravestone.getGraveOwner();
             if ((graveOwner != null && graveOwner.gameProfile().getId().equals(player.getGameProfile().getId())) || !Gravestones.GRAVESTONE_ACCESSIBLE_OWNER_ONLY.getValue()) {
                 String uuid = "";
