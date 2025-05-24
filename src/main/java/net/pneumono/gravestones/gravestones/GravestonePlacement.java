616f5fff94d8c89737dd66044e10cc1999677dcf
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-05-24 14:54:10 +0100

    Improve grave placement logic (fix #24)

--- src/main/java/net/pneumono/gravestones/gravestones/GravestonePlacement.java
+++ src/main/java/net/pneumono/gravestones/gravestones/GravestonePlacement.java
@@ -18,24 +18,53 @@ public class GravestonePlacement {
         return placeGravestoneAtValidPos(world, blockPos.withY(MathHelper.clamp(blockPos.getY(), dimension.minY(), dimension.minY() + dimension.height())));
     }
 
-    private static BlockPos placeGravestoneAtValidPos(World world, BlockPos blockPos) {
-        if (hasNoIrreplaceableBlocks(world, blockPos)) {
-            return placeGravestoneAtPos(world, blockPos);
+    private static BlockPos placeGravestoneAtValidPos(World world, BlockPos origin) {
+        if (getCost(world, origin, origin) == 0) {
+            return placeGravestoneAtPos(world, origin);
         }
 
-        int[] positionOrder = new int[]{0, 1, -1};
-        for (int x : positionOrder) {
-            for (int z : positionOrder) {
-                for (int y : positionOrder) {
-                    BlockPos newPos = blockPos.add(x, y, z);
-                    if (hasNoIrreplaceableBlocks(world, newPos)) {
-                        return placeGravestoneAtPos(world, newPos);
-                    }
-                }
+        double bestCost = -1;
+        BlockPos bestPos = null;
+
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+        for (int x = -2; x <= 2; ++x) for (int y = -2; y <= 2; ++y) for (int z = -2; z <= 2; ++z) {
+            mutable.set(origin.add(x, y, z));
+
+            double cost = getCost(world, mutable, origin);
+            if (cost < bestCost || (bestCost == -1 && cost != -1)) {
+                bestCost = cost;
+                bestPos = mutable.mutableCopy();
             }
         }
 
-        return placeGravestoneAtPos(world, blockPos);
+        if (bestCost == -1) {
+            return null;
+        } else {
+            return placeGravestoneAtPos(world, bestPos);
+        }
+    }
+
+    private static double getCost(World world, BlockPos newPos, BlockPos origin) {
+        BlockState state = world.getBlockState(newPos);
+        Block block = state.getBlock();
+        if (block.getHardness() < 0 || block.getBlastResistance() >= 3600000 || block == Blocks.VOID_AIR) {
+            return -1;
+        }
+
+        double cost = 0;
+        if (!state.getFluidState().isEmpty()) {
+            if (state.getFluidState().isIn(FluidTags.WATER)) {
+                cost += 1.5;
+            } else {
+                cost += 5;
+            }
+        }
+        if (!state.isAir()) cost += state.isReplaceable() ? 1.25 : 5;
+        if (world.getBlockState(newPos.down()).isAir()) cost += 1.5;
+
+        cost += origin.getSquaredDistance(newPos);
+
+        return cost;
     }
 
     private static BlockPos placeGravestoneAtPos(World world, BlockPos blockPos) {
@@ -48,10 +77,5 @@ public class GravestonePlacement {
         world.breakBlock(blockPos, true);
         world.setBlockState(blockPos, gravestoneBlock);
         return blockPos;
-    }
-
-    private static boolean hasNoIrreplaceableBlocks(World world, BlockPos blockPos) {
-        Block block = world.getBlockState(blockPos).getBlock();
-        return !(block.getHardness() < 0 || block.getBlastResistance() >= 3600000) && block != Blocks.VOID_AIR;
     }
 }
