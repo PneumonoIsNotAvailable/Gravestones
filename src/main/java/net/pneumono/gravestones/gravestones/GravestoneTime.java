ab7615b25cd0db7a7e5e6a32114d1453573a1b4e
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-06-01 05:12:53 +0100

    Change file saving date format to the US format

--- src/main/java/net/pneumono/gravestones/gravestones/GravestoneTime.java
+++ src/main/java/net/pneumono/gravestones/gravestones/GravestoneTime.java
@@ -6,8 +6,11 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class GravestoneTime {
+    // Inconsistent date formats suck ass, but it's for the best here, I think.
+    // READABLE needs to stay as it is for backwards compatibility.
+    // FILE_SAVING should be in the US format, since that's (unfortunately) standard, and it actually gets viewed by the player.
     public static final SimpleDateFormat READABLE = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-    public static final SimpleDateFormat FILE_SAVING = new SimpleDateFormat("dd-MM-yyyy_HH-mm-ss");
+    public static final SimpleDateFormat FILE_SAVING = new SimpleDateFormat("MM-dd-yyyy_HH-mm-ss");
 
     public static long getDifferenceInSeconds(String aTime, String bTime) {
         SimpleDateFormat formatter = READABLE;
