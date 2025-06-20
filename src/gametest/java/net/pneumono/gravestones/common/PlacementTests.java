aaec416d6a5c1873d73011108991b100d0a77379
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-06-20 18:57:48 +0100

    Add more tests

--- src/gametest/java/net/pneumono/gravestones/common/PlacementTests.java
+++ src/gametest/java/net/pneumono/gravestones/common/PlacementTests.java
@@ -1,9 +1,13 @@
 package net.pneumono.gravestones.common;
 
 import net.fabricmc.fabric.api.gametest.v1.GameTest;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.test.TestContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.pneumono.gravestones.block.TechnicalGravestoneBlock;
 import net.pneumono.gravestones.content.GravestonesRegistry;
 import net.pneumono.gravestones.util.TestsUtil;
@@ -11,22 +15,22 @@ import net.pneumono.gravestones.util.TestsUtil;
 @SuppressWarnings("unused")
 public class PlacementTests {
     @GameTest(
-            structure = "gravestones_test:spawn_at_death_pos"
+            structure = "gravestones_test:spawns_at_death_pos"
     )
     public void graveSpawnsAtDeathPos(TestContext context) {
-        BlockPos pos = new BlockPos(1, 1, 1);
+        BlockPos pos = TestsUtil.createPos(1);
 
         TestsUtil.createAndKillPlayer(context, pos);
 
-        context.checkBlock(pos, block -> block instanceof TechnicalGravestoneBlock, block -> Text.literal("Expected Gravestone to have been placed! Instead found ").append(block.getName()));
+        context.expectBlock(GravestonesRegistry.GRAVESTONE_TECHNICAL, pos);
         context.complete();
     }
 
     @GameTest(
-            structure = "gravestones_test:adjust_position"
+            structure = "gravestones_test:adjusts_position"
     )
     public void graveAdjustsPosition(TestContext context) {
-        BlockPos pos = new BlockPos(1, 1, 1);
+        BlockPos pos = TestsUtil.createPos(1);
 
         TestsUtil.createAndKillPlayer(context, pos);
 
@@ -38,7 +42,7 @@ public class PlacementTests {
             structure = "gravestones_test:cannot_place"
     )
     public void graveCannotPlace(TestContext context) {
-        BlockPos pos = new BlockPos(3, 3, 3);
+        BlockPos pos = TestsUtil.createPos(3);
 
         TestsUtil.createAndKillPlayer(context, pos);
 
@@ -46,6 +50,60 @@ public class PlacementTests {
         for (int x = 0; x < 5; ++x) for (int y = 0; y < 5; ++y) for (int z = 0; z < 5; ++z) {
             context.dontExpectBlock(GravestonesRegistry.GRAVESTONE_TECHNICAL, mutable.set(x, y, z));
         }
+        context.complete();
+    }
+
+    @GameTest(
+            structure = "gravestones_test:is_waterlogged"
+    )
+    public void graveIsWaterlogged(TestContext context) {
+        BlockPos pos = TestsUtil.createPos(3);;
+
+        TestsUtil.createAndKillPlayer(context, pos);
+
+        context.checkBlockState(pos, state -> state.get(TechnicalGravestoneBlock.WATERLOGGED, false), state -> Text.literal("Expected Gravestone to be waterlogged! Instead found " + state.toString()));
+        context.complete();
+    }
+
+    @GameTest(
+            structure = "gravestones_test:can_find_shore"
+    )
+    public void graveCanFindShore(TestContext context) {
+        BlockPos pos = new BlockPos(2, 1, 2);
+
+        TestsUtil.createAndKillPlayer(context, pos);
+
+        context.expectBlock(GravestonesRegistry.GRAVESTONE_TECHNICAL, pos.east(2).up());
+        context.complete();
+    }
+
+    @GameTest(
+            structure = "gravestones_test:must_replace"
+    )
+    public void graveMustReplace(TestContext context) {
+        BlockPos pos = TestsUtil.createPos(3);
+
+        TestsUtil.createAndKillPlayer(context, pos);
+
+        context.expectBlock(GravestonesRegistry.GRAVESTONE_TECHNICAL, pos);
+        context.expectEntity(EntityType.ITEM);
+        context.complete();
+    }
+
+    @GameTest(
+            structure = "gravestones_test:resists_explosion"
+    )
+    public void graveResistsExplosion(TestContext context) {
+        ServerWorld world = context.getWorld();
+        BlockPos pos = TestsUtil.createPos(1);
+
+        PlayerEntity player = TestsUtil.createPlayer(context, pos);
+
+        BlockPos absolutePos = context.getAbsolutePos(pos);
+        world.createExplosion(null, absolutePos.getX(), absolutePos.getY(), absolutePos.getZ(), 5, World.ExplosionSourceType.NONE);
+
+        context.expectBlock(GravestonesRegistry.GRAVESTONE_TECHNICAL, pos);
+        context.dontExpectEntity(EntityType.ITEM);
         context.complete();
     }
 }
--- src/gametest/java/net/pneumono/gravestones/util/TestsUtil.java
+++ src/gametest/java/net/pneumono/gravestones/util/TestsUtil.java
@@ -1,18 +1,52 @@
 package net.pneumono.gravestones.util;
 
-import net.minecraft.block.Blocks;
+import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.test.TestContext;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameMode;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
 
 public class TestsUtil {
+    public static BlockPos createPos(int i) {
+        return new BlockPos(i, i, i);
+    }
+
     public static void createAndKillPlayer(TestContext context, BlockPos pos) {
+        createAndKillPlayer(context, pos, UUID.randomUUID());
+    }
+
+    public static void createAndKillPlayer(TestContext context, BlockPos pos, UUID uuid) {
+        PlayerEntity player = createPlayer(context, pos, uuid);
+        player.kill(context.getWorld());
+    }
+
+    public static PlayerEntity createPlayer(TestContext context, BlockPos pos) {
+        return createPlayer(context, pos, UUID.randomUUID());
+    }
+
+    public static PlayerEntity createPlayer(TestContext context, BlockPos pos, UUID uuid) {
         ServerWorld world = context.getWorld();
-        PlayerEntity player = context.createMockPlayer(GameMode.SURVIVAL);
+
+        PlayerEntity player = new PlayerEntity(world, new GameProfile(uuid, "test-mock-player")) {
+            @NotNull
+            @Override
+            public GameMode getGameMode() {
+                return GameMode.SURVIVAL;
+            }
+
+            @Override
+            public boolean isControlledByPlayer() {
+                return false;
+            }
+        };
+
         world.spawnEntity(player);
         player.refreshPositionAndAngles(context.getAbsolutePos(pos), 0, 0);
-        player.kill(world);
+
+        return player;
     }
 }
