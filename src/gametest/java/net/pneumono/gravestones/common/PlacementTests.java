55e4fdeead3c3e229a0642c45290bfc0e53f46bc
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-06-20 18:05:43 +0100

    Add cannot place test and add structures

--- src/gametest/java/net/pneumono/gravestones/common/PlacementTests.java
+++ src/gametest/java/net/pneumono/gravestones/common/PlacementTests.java
@@ -1,8 +1,6 @@
 package net.pneumono.gravestones.common;
 
 import net.fabricmc.fabric.api.gametest.v1.GameTest;
-import net.minecraft.block.Blocks;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.test.TestContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
@@ -12,12 +10,11 @@ import net.pneumono.gravestones.util.TestsUtil;
 
 @SuppressWarnings("unused")
 public class PlacementTests {
-    @GameTest()
-    public void gravestonesAppear(TestContext context) {
-        ServerWorld world = context.getWorld();
-        BlockPos pos = new BlockPos(0, 1, 0);
-
-        context.setBlockState(pos.down(), Blocks.POLISHED_ANDESITE.getDefaultState());
+    @GameTest(
+            structure = "gravestones_test:spawn_at_death_pos"
+    )
+    public void gravestonesSpawnAtDeathPos(TestContext context) {
+        BlockPos pos = new BlockPos(1, 1, 1);
 
         TestsUtil.createAndKillPlayer(context, pos);
 
@@ -25,17 +22,30 @@ public class PlacementTests {
         context.complete();
     }
 
-    @GameTest()
+    @GameTest(
+            structure = "gravestones_test:adjust_position"
+    )
     public void gravestonesAdjustPosition(TestContext context) {
-        ServerWorld world = context.getWorld();
-
-        BlockPos pos = new BlockPos(0, 1, 0);
-
-        context.setBlockState(pos, Blocks.POLISHED_ANDESITE.getDefaultState());
+        BlockPos pos = new BlockPos(1, 1, 1);
 
         TestsUtil.createAndKillPlayer(context, pos);
 
         context.dontExpectBlock(GravestonesRegistry.GRAVESTONE_TECHNICAL, pos);
+        context.complete();
+    }
+
+    @GameTest(
+            structure = "gravestones_test:cannot_place"
+    )
+    public void graveCannotPlace(TestContext context) {
+        BlockPos pos = new BlockPos(3, 3, 3);
+
+        TestsUtil.createAndKillPlayer(context, pos);
+
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+        for (int x = 0; x < 5; ++x) for (int y = 0; y < 5; ++y) for (int z = 0; z < 5; ++z) {
+            context.dontExpectBlock(GravestonesRegistry.GRAVESTONE_TECHNICAL, mutable.set(x, y, z));
+        }
         context.complete();
     }
 }
