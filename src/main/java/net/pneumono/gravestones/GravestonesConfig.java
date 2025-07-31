e557a880ae481a5c8a440a44bf822fae5671e0ce
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-07-31 13:34:14 +0100

    Update configs to new system

--- src/main/java/net/pneumono/gravestones/GravestonesConfig.java
+++ src/main/java/net/pneumono/gravestones/GravestonesConfig.java
@@ -3,77 +3,75 @@ package net.pneumono.gravestones;
 import net.pneumono.gravestones.gravestones.enums.DecayTimeType;
 import net.pneumono.gravestones.gravestones.enums.ExperienceKeptCalculation;
 import net.pneumono.gravestones.gravestones.enums.TimeFormat;
-import net.pneumono.pneumonocore.config.*;
+import net.pneumono.pneumonocore.config_api.ConfigApi;
+import net.pneumono.pneumonocore.config_api.configurations.*;
+import net.pneumono.pneumonocore.config_api.enums.LoadType;
+import net.pneumono.pneumonocore.config_api.enums.TimeUnit;
 
 public class GravestonesConfig {
-    public static final BooleanConfiguration DECAY_WITH_DEATHS = new BooleanConfiguration(Gravestones.MOD_ID, "decay_with_deaths", ConfigEnv.SERVER, true);
-    public static final BooleanConfiguration DECAY_WITH_TIME = new BooleanConfiguration(Gravestones.MOD_ID, "decay_with_time", ConfigEnv.SERVER, true);
-    public static final TimeConfiguration DECAY_TIME = new TimeConfiguration(Gravestones.MOD_ID, "decay_time", ConfigEnv.SERVER, 8L * TimeUnit.HOURS.getDivision());
-    public static final EnumConfiguration<DecayTimeType> DECAY_TIME_TYPE = new EnumConfiguration<>(Gravestones.MOD_ID, "decay_time_type", ConfigEnv.SERVER, DecayTimeType.TICKS);
-    public static final BooleanConfiguration AESTHETIC_DECAY = new BooleanConfiguration(Gravestones.MOD_ID, "aesthetic_decay", ConfigEnv.SERVER, false);
+    public static final BooleanConfiguration DECAY_WITH_DEATHS = register("decay_with_deaths", new BooleanConfiguration(
+            true, new ConfigSettings().category("decay").loadType(LoadType.INSTANT)
+    ));
+    public static final BooleanConfiguration DECAY_WITH_TIME = register("decay_with_time", new BooleanConfiguration(
+            true, new ConfigSettings().category("decay").loadType(LoadType.INSTANT)
+    ));
+    public static final TimeConfiguration DECAY_TIME = register("decay_time", new TimeConfiguration(
+            8L * TimeUnit.HOURS.getDivision(), new ConfigSettings().category("decay").loadType(LoadType.INSTANT).parent(DECAY_WITH_TIME, value -> value)
+    ));
+    public static final EnumConfiguration<DecayTimeType> DECAY_TIME_TYPE = register("decay_time_type", new EnumConfiguration<>(
+            DecayTimeType.TICKS, new ConfigSettings().category("decay").loadType(LoadType.INSTANT).parent(DECAY_WITH_TIME, value -> value)
+    ));
+    public static final BooleanConfiguration AESTHETIC_DECAY = register("aesthetic_decay", new BooleanConfiguration(
+            false, new ConfigSettings().category("decay").loadType(LoadType.INSTANT).parent(DECAY_WITH_TIME, value -> value)
+    ));
 
-    public static final BooleanConfiguration STORE_EXPERIENCE = new BooleanConfiguration(Gravestones.MOD_ID, "store_experience", ConfigEnv.SERVER, true);
-    public static final BooleanConfiguration EXPERIENCE_CAP = new BooleanConfiguration(Gravestones.MOD_ID, "experience_cap", ConfigEnv.SERVER, true);
-    public static final EnumConfiguration<ExperienceKeptCalculation> EXPERIENCE_KEPT = new EnumConfiguration<>(Gravestones.MOD_ID, "experience_kept", ConfigEnv.SERVER, ExperienceKeptCalculation.VANILLA);
-    public static final BooleanConfiguration EXPERIENCE_DECAY = new BooleanConfiguration(Gravestones.MOD_ID, "experience_decay", ConfigEnv.SERVER, false);
-    public static final BooleanConfiguration DROP_EXPERIENCE = new BooleanConfiguration(Gravestones.MOD_ID, "drop_experience", ConfigEnv.SERVER, false);
+    public static final BooleanConfiguration STORE_EXPERIENCE = register("store_experience", new BooleanConfiguration(
+            true, new ConfigSettings().category("experience").loadType(LoadType.INSTANT)
+    ));
+    public static final BooleanConfiguration EXPERIENCE_CAP = register("experience_cap", new BooleanConfiguration(
+            true, new ConfigSettings().category("experience").loadType(LoadType.INSTANT).parent(STORE_EXPERIENCE, value -> value)
+    ));
+    public static final EnumConfiguration<ExperienceKeptCalculation> EXPERIENCE_KEPT = register("experience_kept", new EnumConfiguration<>(
+            ExperienceKeptCalculation.VANILLA, new ConfigSettings().category("experience").loadType(LoadType.INSTANT).parent(STORE_EXPERIENCE, value -> value)
+    ));
+    public static final BooleanConfiguration EXPERIENCE_DECAY = register("experience_decay", new BooleanConfiguration(
+            false, new ConfigSettings().category("experience").loadType(LoadType.INSTANT).parent(STORE_EXPERIENCE, value -> value)
+    ));
+    public static final BooleanConfiguration DROP_EXPERIENCE = register("drop_experience", new BooleanConfiguration(
+            false, new ConfigSettings().category("experience").loadType(LoadType.INSTANT).parent(STORE_EXPERIENCE, value -> value)
+    ));
 
-    public static final BooleanConfiguration GRAVESTONE_ACCESSIBLE_OWNER_ONLY = new BooleanConfiguration(Gravestones.MOD_ID, "gravestone_accessible_owner_only", ConfigEnv.SERVER, true);
-    public static final BooleanConfiguration BROADCAST_COLLECT_IN_CHAT = new BooleanConfiguration(Gravestones.MOD_ID, "broadcast_collect_in_chat", ConfigEnv.SERVER, false);
-    public static final BooleanConfiguration BROADCAST_COORDINATES_IN_CHAT = new BooleanConfiguration(Gravestones.MOD_ID, "broadcast_coordinates_in_chat", ConfigEnv.SERVER, false);
+    public static final BooleanConfiguration GRAVESTONE_ACCESSIBLE_OWNER_ONLY = register("gravestone_accessible_owner_only", new BooleanConfiguration(
+            true, new ConfigSettings().category("multiplayer").loadType(LoadType.INSTANT)
+    ));
+    public static final BooleanConfiguration BROADCAST_COLLECT_IN_CHAT = register("broadcast_collect_in_chat", new BooleanConfiguration(
+            false, new ConfigSettings().category("multiplayer").loadType(LoadType.INSTANT)
+    ));
+    public static final BooleanConfiguration BROADCAST_COORDINATES_IN_CHAT = register("broadcast_coordinates_in_chat", new BooleanConfiguration(
+            false, new ConfigSettings().category("multiplayer").loadType(LoadType.INSTANT)
+    ));
 
-    public static final BooleanConfiguration AESTHETIC_GRAVESTONES = new BooleanConfiguration(Gravestones.MOD_ID, "aesthetic_gravestones", ConfigEnv.SERVER, true);
-    public static final BooleanConfiguration SPAWN_GRAVESTONE_SKELETONS = new BooleanConfiguration(Gravestones.MOD_ID, "spawn_gravestone_skeletons", ConfigEnv.SERVER, false);
-    public static final BooleanConfiguration SHOW_HEADS = new BooleanConfiguration(Gravestones.MOD_ID, "show_heads", ConfigEnv.CLIENT, true);
-    public static final EnumConfiguration<TimeFormat> TIME_FORMAT = new EnumConfiguration<>(Gravestones.MOD_ID, "time_format", ConfigEnv.CLIENT, TimeFormat.MMDDYYYY);
-    public static final BooleanConfiguration CONSOLE_INFO = new BooleanConfiguration(Gravestones.MOD_ID, "console_info", ConfigEnv.CLIENT, false);
+    public static final BooleanConfiguration AESTHETIC_GRAVESTONES = register("aesthetic_gravestones", new BooleanConfiguration(
+            true, new ConfigSettings().loadType(LoadType.RESTART)
+    ));
+    public static final BooleanConfiguration SPAWN_GRAVESTONE_SKELETONS = register("spawn_gravestone_skeletons", new BooleanConfiguration(
+            false, new ConfigSettings().loadType(LoadType.INSTANT)
+    ));
+    public static final BooleanConfiguration SHOW_HEADS = register("show_heads", new BooleanConfiguration(
+            true, new ConfigSettings().clientSide().category("clientside").loadType(LoadType.INSTANT)
+    ));
+    public static final EnumConfiguration<TimeFormat> TIME_FORMAT = register("time_format", new EnumConfiguration<>(
+            TimeFormat.MMDDYYYY, new ConfigSettings().clientSide().category("clientside").loadType(LoadType.INSTANT)
+    ));
+    public static final BooleanConfiguration CONSOLE_INFO = register("console_info", new BooleanConfiguration(
+            false, new ConfigSettings().clientSide().category("clientside").loadType(LoadType.INSTANT)
+    ));
+
+    public static <T extends AbstractConfiguration<?>> T register(String name, T config) {
+        return ConfigApi.register(Gravestones.id(name), config);
+    }
 
     public static void registerGravestonesConfigs() {
-        Configs.register(Gravestones.MOD_ID,
-                DECAY_WITH_DEATHS,
-                DECAY_WITH_TIME,
-                DECAY_TIME,
-                DECAY_TIME_TYPE,
-                AESTHETIC_DECAY,
-                STORE_EXPERIENCE,
-                EXPERIENCE_CAP,
-                EXPERIENCE_KEPT,
-                EXPERIENCE_DECAY,
-                DROP_EXPERIENCE,
-                GRAVESTONE_ACCESSIBLE_OWNER_ONLY,
-                BROADCAST_COLLECT_IN_CHAT,
-                BROADCAST_COORDINATES_IN_CHAT,
-                AESTHETIC_GRAVESTONES,
-                SPAWN_GRAVESTONE_SKELETONS,
-                CONSOLE_INFO,
-                SHOW_HEADS,
-                TIME_FORMAT
-        );
-        Configs.registerCategories(Gravestones.MOD_ID,
-                new ConfigCategory(Gravestones.MOD_ID, "decay",
-                        DECAY_WITH_DEATHS,
-                        DECAY_WITH_TIME,
-                        DECAY_TIME,
-                        DECAY_TIME_TYPE,
-                        AESTHETIC_DECAY
-                ),
-                new ConfigCategory(Gravestones.MOD_ID, "experience",
-                        STORE_EXPERIENCE,
-                        EXPERIENCE_CAP,
-                        EXPERIENCE_KEPT,
-                        EXPERIENCE_DECAY,
-                        DROP_EXPERIENCE
-                ),
-                new ConfigCategory(Gravestones.MOD_ID, "multiplayer",
-                        GRAVESTONE_ACCESSIBLE_OWNER_ONLY,
-                        BROADCAST_COLLECT_IN_CHAT,
-                        BROADCAST_COORDINATES_IN_CHAT
-                ),
-                new ConfigCategory(Gravestones.MOD_ID, "clientside",
-                        SHOW_HEADS,
-                        TIME_FORMAT,
-                        CONSOLE_INFO
-                )
-        );
+        ConfigApi.finishRegistry(Gravestones.MOD_ID);
     }
 }
