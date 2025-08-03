a23db2c72f95e112dbe3a3299c68ec36afc3eb4c
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-08-03 19:07:20 +0100

    Remove unnecessary mixin extension

--- src/main/java/net/pneumono/gravestones/mixin/LivingEntityMixin.java
+++ src/main/java/net/pneumono/gravestones/mixin/LivingEntityMixin.java
@@ -1,12 +1,9 @@
 package net.pneumono.gravestones.mixin;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.world.World;
 import net.pneumono.gravestones.gravestones.GravestoneCreation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,11 +11,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin extends Entity {
-    protected LivingEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
-        super(entityType, world);
-    }
-
+public abstract class LivingEntityMixin {
     @Inject(method = "drop", at = @At("HEAD"))
     public void spawnGravestone(ServerWorld world, DamageSource damageSource, CallbackInfo ci) {
         if ((Object)this instanceof PlayerEntity player) {
