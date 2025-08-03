586c453686285618a442577b0978ada0437cfbd6
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-08-03 19:06:33 +0100

    Fix Accessories Trinkets Compat Layer not working with Gravestones
    Accessories isn't updated to this version but still

---
+++ src/main/java/net/pneumono/gravestones/mixin/LivingEntityMixin.java
@@ -0,0 +1,21 @@
+package net.pneumono.gravestones.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.pneumono.gravestones.gravestones.GravestoneCreation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
+    @Inject(method = "drop", at = @At("HEAD"))
+    public void spawnGravestone(ServerWorld world, DamageSource damageSource, CallbackInfo ci) {
+        if ((Object)this instanceof PlayerEntity player) {
+            GravestoneCreation.handleGravestones(player);
+        }
+    }
+}
