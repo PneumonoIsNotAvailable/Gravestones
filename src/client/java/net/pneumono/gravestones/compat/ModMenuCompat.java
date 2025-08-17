1051d77197e18d42331933b3c016d1284c28eeeb
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-08-17 06:00:23 +0100

    Add Mod Menu link for the Wiki and move Mod Menu compat class

--- src/client/java/net/pneumono/gravestones/compat/ModMenuCompat.java
+++ src/client/java/net/pneumono/gravestones/compat/ModMenuCompat.java
@@ -1,10 +1,11 @@
-package net.pneumono.gravestones;
+package net.pneumono.gravestones.compat;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.pneumono.gravestones.Gravestones;
 import net.pneumono.pneumonocore.PneumonoCoreModMenu;
 
-public class GravestonesModMenu implements ModMenuApi {
+public class ModMenuCompat implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return PneumonoCoreModMenu.getModConfigScreenFactory(Gravestones.MOD_ID);
