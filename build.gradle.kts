5b26d5c445b7801edcc2182dae98184016de7f71
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-10-10 02:53:31 +0100

    Change format of supported versions in gradle properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	id("fabric-loom") version "1.11-SNAPSHOT"
 	id("maven-publish")
+	id("me.modmuss50.mod-publish-plugin") version "1.0.0"
 }
 
 val javaVersion = if (stonecutter.eval(stonecutter.current.version, ">=1.20.5"))
@@ -100,13 +101,15 @@ dependencies {
 tasks {
 	processResources {
 		inputs.property("version", stonecutter.current.version)
-		inputs.property("supported", project.property("supported_versions"))
+		inputs.property("min_supported", project.property("min_supported_version"))
+		inputs.property("max_supported", project.property("max_supported_version"))
 
 		filesMatching("fabric.mod.json") {
 			expand(
 				mutableMapOf(
 					"version" to stonecutter.current.version,
-					"supported" to project.property("supported_versions")
+					"min_supported" to project.property("min_supported_version"),
+					"max_supported" to project.property("max_supported_version")
 				)
 			)
 		}
