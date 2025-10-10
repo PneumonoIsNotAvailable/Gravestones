34a7b22587678585d62f9019f2bacd641cd35a9a
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-10-10 02:44:26 +0100

    Fix Accessories cosmetic slots not being put into graves (#64)

--- src/main/java/net/pneumono/gravestones/compat/AccessoriesDataType.java
+++ src/main/java/net/pneumono/gravestones/compat/AccessoriesDataType.java
@@ -12,7 +12,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.pneumono.gravestones.api.GravestoneDataType;
@@ -21,38 +20,38 @@ import net.pneumono.gravestones.multiversion.VersionUtil;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Map;
 
 //? if >=1.21.5 {
-/^import io.wispforest.accessories.api.core.Accessory;
+import io.wispforest.accessories.api.core.Accessory;
 import io.wispforest.accessories.api.core.AccessoryRegistry;
 import io.wispforest.accessories.api.slot.SlotPredicateRegistry;
 import io.wispforest.accessories.impl.core.ExpandedContainer;
-^///?} else if >=1.21.3 {
+//?} else if >=1.21.3 {
 /^import io.wispforest.accessories.api.Accessory;
 import io.wispforest.accessories.api.AccessoryRegistry;
 import io.wispforest.accessories.api.slot.SlotPredicateRegistry;
 import io.wispforest.accessories.impl.ExpandedSimpleContainer;
 ^///?} else {
-import io.wispforest.accessories.api.Accessory;
+/^import io.wispforest.accessories.api.Accessory;
 import io.wispforest.accessories.api.AccessoriesAPI;
 import io.wispforest.accessories.impl.ExpandedSimpleContainer;
-//?}
+^///?}
 
 //? if >=1.21.4 {
-/^import io.wispforest.accessories.Accessories;
-^///?}
+import io.wispforest.accessories.Accessories;
+//?}
 
 public class AccessoriesDataType extends GravestoneDataType {
     private static final String KEY = "accessories";
 
     //? if >=1.21.4 {
-    /^@SuppressWarnings("UnstableApiUsage")
-    ^///?}
+    @SuppressWarnings("UnstableApiUsage")
+    //?}
     @Override
     public void writeData(NbtCompound nbt, DynamicOps<NbtElement> ops, PlayerEntity player) throws Exception {
         MinecraftServer server = player.getServer();
-        if (server != null /^? if >=1.21.4 {^//^&& server.getGameRules().getBoolean(Accessories.RULE_KEEP_ACCESSORY_INVENTORY)^//^?}^/) {
+        if (server != null /^? if >=1.21.4 {^/&& server.getGameRules().getBoolean(Accessories.RULE_KEEP_ACCESSORY_INVENTORY)/^?}^/) {
             return;
         }
 
@@ -61,14 +60,37 @@ public class AccessoriesDataType extends GravestoneDataType {
             throw new IllegalStateException("Player {} does not have an AccessoriesCapability");
         }
 
-        List<SlotReferencePrimitive> list = capability.getAllEquipped().stream()
-                .filter(reference -> !GravestonesApi.shouldSkipItem(player, reference.stack()))
-                .map(reference -> {
-                    ItemStack stack = reference.stack();
-                    reference.reference().setStack(ItemStack.EMPTY);
-                    return new SlotReferencePrimitive(stack, reference.reference());
-                })
-                .toList();
+        List<SlotReferencePrimitive> list = new ArrayList<>();
+        for (Map.Entry<String, AccessoriesContainer> entry : capability.getContainers().entrySet()) {
+            String name = entry.getKey();
+            AccessoriesContainer container = entry.getValue();
+
+            //? if >=1.21.5 {
+            ExpandedContainer accessories = container.getAccessories();
+            //?} else {
+            /^ExpandedSimpleContainer accessories = container.getAccessories();
+            ^///?}
+            for (int index = 0; index < accessories.size(); ++index) {
+                ItemStack stack = accessories.getStack(index);
+                if (!GravestonesApi.shouldSkipItem(player, stack) && !stack.isEmpty()) {
+                    accessories.removeStack(index);
+                    list.add(new SlotReferencePrimitive(stack, name, index, false));
+                }
+            }
+
+            //? if >=1.21.5 {
+            ExpandedContainer cosmetics = container.getCosmeticAccessories();
+            //?} else {
+            /^ExpandedSimpleContainer cosmetics = container.getCosmeticAccessories();
+            ^///?}
+            for (int index = 0; index < cosmetics.size(); ++index) {
+                ItemStack stack = cosmetics.getStack(index);
+                if (!GravestonesApi.shouldSkipItem(player, stack) && !stack.isEmpty()) {
+                    cosmetics.removeStack(index);
+                    list.add(new SlotReferencePrimitive(stack, name, index, true));
+                }
+            }
+        }
 
         VersionUtil.put(ops, nbt, KEY, SlotReferencePrimitive.CODEC.listOf(), list);
     }
@@ -94,104 +116,77 @@ public class AccessoriesDataType extends GravestoneDataType {
         }
 
         List<ItemStack> remaining = new ArrayList<>();
-
         for (SlotReferencePrimitive primitive : list) {
             ItemStack newStack = primitive.stack;
             if (newStack.isEmpty()) continue;
             int index = primitive.index;
 
-            AccessoriesContainer container = capability.getContainers().get(primitive.slotName);
+            AccessoriesContainer container = capability.getContainers().get(primitive.slotName());
             if (container == null || container.getSize() <= 0) {
                 remaining.add(newStack);
                 continue;
             }
 
+            SlotReference reference = SlotReference.of(player, primitive.slotName(), primitive.index());
+
             boolean canInsert =
             //? if >=1.21.3 {
-            /^SlotPredicateRegistry.canInsertIntoSlot(newStack, container.createReference(primitive.index));
-            ^///?} else {
-            AccessoriesAPI.canInsertIntoSlot(newStack, container.createReference(primitive.index));
-            //?}
+            SlotPredicateRegistry.canInsertIntoSlot(newStack, reference);
+            //?} else {
+            /^AccessoriesAPI.canInsertIntoSlot(newStack, reference);
+            ^///?}
             if (!canInsert) {
                 remaining.add(newStack);
                 continue;
             }
 
             //? if >=1.21.5 {
-            /^ExpandedContainer accessories = container.getAccessories();
-            ^///?} else {
-            ExpandedSimpleContainer accessories = container.getAccessories();
-            //?}
+            ExpandedContainer accessories = primitive.cosmetic ? container.getCosmeticAccessories() : container.getAccessories();
+            //?} else {
+            /^ExpandedSimpleContainer accessories = container.getAccessories();
+            ^///?}
 
             ItemStack oldStack = accessories.getStack(index);
-            SlotReference slotReference = container.createReference(index);
 
             boolean canUnequipOldStack =
             //? if >=1.21.3 {
-            /^AccessoryRegistry.canUnequip(oldStack, slotReference);
-            ^///?} else {
-            AccessoriesAPI.canUnequip(oldStack, slotReference);
-            //?}
+            AccessoryRegistry.canUnequip(oldStack, reference);
+            //?} else {
+            /^AccessoriesAPI.canUnequip(oldStack, reference);
+            ^///?}
             boolean canInsertNewStack =
             //? if >=1.21.3 {
-            /^SlotPredicateRegistry.canInsertIntoSlot(newStack, slotReference);
-            ^///?} else {
-            AccessoriesAPI.canInsertIntoSlot(newStack, slotReference);
-            //?}
+            SlotPredicateRegistry.canInsertIntoSlot(newStack, reference);
+            //?} else {
+            /^AccessoriesAPI.canInsertIntoSlot(newStack, reference);
+            ^///?}
 
             if (oldStack.isEmpty() && canUnequipOldStack && canInsertNewStack
             ) {
                 Accessory accessory =
                 //? if >=1.21.3 {
-                /^AccessoryRegistry.getAccessoryOrDefault(oldStack);
-                ^///?} else {
-                AccessoriesAPI.getOrDefaultAccessory(oldStack);
-                //?}
+                AccessoryRegistry.getAccessoryOrDefault(oldStack);
+                //?} else {
+                /^AccessoriesAPI.getOrDefaultAccessory(oldStack);
+                ^///?}
                 ItemStack splitStack = newStack.split(accessory.maxStackSize(newStack));
-
-                slotReference.setStack(splitStack);
+                accessories.setStack(index, splitStack);
+                if (!newStack.isEmpty()) {
+                    remaining.add(newStack);
+                }
             }
         }
 
         dropStacks(world, pos, remaining);
     }
 
-    //? if >=1.21.5 {
-    /^public record SlotReferencePrimitive(ItemStack stack, String slotName, int index, List<Integer> innerIndices, boolean isNested) {
+    public record SlotReferencePrimitive(ItemStack stack, String slotName, int index, boolean cosmetic) {
         public static final Codec<SlotReferencePrimitive> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-                ItemStack.CODEC.fieldOf("newStack").forGetter(SlotReferencePrimitive::stack),
+                ItemStack.CODEC.fieldOf("stack").forGetter(SlotReferencePrimitive::stack),
                 Codec.STRING.fieldOf("slot_name").forGetter(SlotReferencePrimitive::slotName),
                 Codec.INT.fieldOf("index").forGetter(SlotReferencePrimitive::index),
-                Codec.INT.listOf().fieldOf("inner_indices").forGetter(SlotReferencePrimitive::innerIndices),
-                Codec.BOOL.fieldOf("nested").forGetter(SlotReferencePrimitive::isNested)
+                Codec.BOOL.fieldOf("cosmetic").forGetter(SlotReferencePrimitive::cosmetic)
         ).apply(instance, SlotReferencePrimitive::new));
-
-        public SlotReferencePrimitive(ItemStack stack, SlotReference reference) {
-            this(
-                    stack,
-                    reference.slotName(),
-                    reference.index(),
-                    reference.innerIndices(),
-                    reference.isNested()
-            );
-        }
     }
-    ^///?} else {
-    public record SlotReferencePrimitive(ItemStack stack, String slotName, int index) {
-        public static final Codec<SlotReferencePrimitive> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-                ItemStack.CODEC.fieldOf("newStack").forGetter(SlotReferencePrimitive::stack),
-                Codec.STRING.fieldOf("slot_name").forGetter(SlotReferencePrimitive::slotName),
-                Codec.INT.fieldOf("index").forGetter(SlotReferencePrimitive::index)
-        ).apply(instance, SlotReferencePrimitive::new));
-
-        public SlotReferencePrimitive(ItemStack stack, SlotReference reference) {
-            this(
-                    stack,
-                    reference.slotName(),
-                    reference.slot()
-            );
-        }
-    }
-    //?}
 }
 *///?}
