6fca6c94fa9828e71085c573bb0f4dba4f8e7380
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2026-01-28 17:37:49 +0000

    Fix Resource Backpacks' items being voided (#88)

--- src/main/java/net/pneumono/gravestones/compat/ResourceBackpacksDataType.java
+++ src/main/java/net/pneumono/gravestones/compat/ResourceBackpacksDataType.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
+import net.minecraft.screen.slot.Slot;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -13,6 +14,7 @@ import net.pneumono.gravestones.api.GravestoneDataType;
 import net.pneumono.gravestones.api.GravestonesApi;
 import net.pneumono.gravestones.multiversion.VersionUtil;
 import net.xstopho.resource_backpacks.backpack.api.BackpackHolder;
+import net.xstopho.resource_backpacks.client.slot.BackpackSlot;
 
 import java.util.Optional;
 
@@ -45,7 +47,12 @@ public class ResourceBackpacksDataType extends GravestoneDataType {
         Optional<ItemStack> optional = VersionUtil.get(ops, nbt, KEY, ItemStack.CODEC);
         optional.ifPresent(stack -> {
             if (player instanceof BackpackHolder backpackHolder && backpackHolder.getBackpack().isEmpty()) {
-                backpackHolder.setBackpack(stack);
+                for (Slot slot : player.playerScreenHandler.slots) {
+                    if (slot instanceof BackpackSlot) {
+                        slot.setStackNoCallbacks(stack);
+                        break;
+                    }
+                }
             } else {
                 dropStack(player, stack);
             }
