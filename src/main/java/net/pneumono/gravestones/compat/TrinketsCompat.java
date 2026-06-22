157ee9ffd6703cd2c740cb4725a9cfd34554919d
suerion <55131959+suerion@users.noreply.github.com>
2026-06-23 00:15:01 +0200

    Uncomment Trinkets registering

--- src/main/java/net/pneumono/gravestones/compat/TrinketsCompat.java
+++ src/main/java/net/pneumono/gravestones/compat/TrinketsCompat.java
@@ -5,8 +5,6 @@ import net.pneumono.gravestones.api.GravestonesApi;
 
 public class TrinketsCompat {
     public static void register() {
-        //? if trinkets {
-        /*GravestonesApi.registerDataType(Gravestones.id("trinkets"), new TrinketsDataType());
-        *///?}
+        GravestonesApi.registerDataType(Gravestones.id("trinkets"), new TrinketsDataType());
     }
 }
